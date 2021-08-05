ea231b0e4538af69cf7df3481f42df85565db520
leiteguitar <85917335+leiteguitar@users.noreply.github.com>
2021-08-05 19:22:04 -0300

    Solution Main

---
+++ ProgramandoComCSharpAgo21/CSharp.Capitulo01.Sintaxe/VariaveisForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Sintaxe
+{
+    public partial class VariaveisForm : Form
+    {
+        int x = 32;
+        int y = 16;
+        int w = 45;
+        int z = 32;
+
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void aritmeticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*Comentário
+                Comentário
+            Comentário*/
+
+            int x = 42;
+            int y;
+            string nome = "Vítor";
+            char letra = 'a';
+            var dataNascimento = new DateTime(2000, 01, 21);
+            bool aprovado = true;
+            var media = 2M/*.3m*/;
+            double distancia = 1.2d;
+            float altura = 0.8f;
+            long populacao = 2013/*L*/;
+
+            var a = 2;
+            var A = 10;
+            int b = 6, c = 10, d = 13;
+
+            //c = 9.9m; // estaticamente tipada.
+
+            decimal bim1 = 2.9m;
+            string @class = "3A";
+
+            object objeto = "texto";
+            objeto = 1;
+
+            //var quantidade = resultadoListBox.Items.Count;
+            resultadoListBox.Items.Clear();
+
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add(string.Concat("b = ", b/*, "c =", c*/));
+            //resultadoListBox.Items.Add(string.Format("c = {0}, d = {1}", c, d));
+            resultadoListBox.Items.Add(string.Format("c = {0}", c));
+            resultadoListBox.Items.Add($"d = {d}");
+
+            resultadoListBox.Items.Add($"c * d = {c * d}");
+            resultadoListBox.Items.Add($"d / a = {d / a}");
+            resultadoListBox.Items.Add($"d % a = {d % a}");
+        }
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //int e = 9;
+
+            resultadoListBox.Items.Clear();
+
+            var x = 5;
+            resultadoListBox.Items.Add("x = " + x);
+
+            //x = x - 3;
+            x -= 3;
+
+            //x =- 3;
+            //x *= 3;
+            //x /= 3;
+
+            resultadoListBox.Items.Add("x = " + x);
+        }
+
+        private void incrementaisDecrementaisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resultadoListBox.Items.Clear();
+
+            int a;
+
+            a = 5;
+            resultadoListBox.Items.Add("Exemplo de pré-incremental");
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add($"2 + ++a = {2 + ++a}"); // a = a + 1
+            resultadoListBox.Items.Add("a = " + a);
+
+            resultadoListBox.Items.Add("-------------------------------------------------------");
+
+            a = 5;
+            resultadoListBox.Items.Add("Exemplo de pós-incremental");
+            resultadoListBox.Items.Add("a = " + a);
+            resultadoListBox.Items.Add($"2 + a++ = {2 + a++}");
+            resultadoListBox.Items.Add("a = " + a); // a = a + 1
+        }
+
+        private void booleanasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibirVariaveis();
+
+            resultadoListBox.Items.Add($"w <= x = {w <= x}");
+            resultadoListBox.Items.Add($"x == z = {x == z}");
+            resultadoListBox.Items.Add($"x != z = {x != z}");
+        }
+
+        private void ExibirVariaveis()
+        {
+            resultadoListBox.Items.Clear();
+
+            resultadoListBox.Items.Add("x = " + x);
+            resultadoListBox.Items.Add("y = " + y);
+            resultadoListBox.Items.Add("w = " + w);
+            resultadoListBox.Items.Add("z = " + z);
+
+            resultadoListBox.Items.Add(new string('-', 50));
+        }
+
+        private void logicasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExibirVariaveis();
+
+            resultadoListBox.Items.Add($"w <= x || y == 16 = {w <= x || y == 16}");
+            resultadoListBox.Items.Add($"x == z && x != z = {x == z && x != z}");
+            resultadoListBox.Items.Add($"!(y > w) = {!(y > w)}");
+        }
+
+        private void ternariasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int ano;
+
+            ano = 2014;
+            resultadoListBox.Items.Add($"O ano {ano} é bissexto? {(ano % 4 == 0 ? "Sim" : "Não")}.");
+
+            ano = 2016;
+            resultadoListBox.Items.Add($"O ano {ano} é bissexto? {(DateTime.IsLeapYear(ano) ? "Sim" : "Não")}.");
+
+            //var resposta = "";
+            //if (DateTime.IsLeapYear(ano))
+            //{
+            //    resposta = "Sim";
+            //}
+            //else
+            //{
+            //    resposta = "Não";
+            //}
+        }
+    }
+}
