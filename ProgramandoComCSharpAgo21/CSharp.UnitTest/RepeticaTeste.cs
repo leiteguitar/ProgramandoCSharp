76d4b00c0c40b04292f31f174f353c7bdf37adf8
Vinny <vih_leite@hotmail.com>
2021-08-06 19:36:25 -0300

    commiting

---
+++ ProgramandoComCSharpAgo21/CSharp.UnitTest/RepeticaTeste.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharp.UnitTest
+{
+    [TestClass]
+    public class RepeticaTeste
+    {
+        [TestMethod]
+        public void TabuadaTest()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i}*{j}={i*j}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ContinueTeste()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i <= 5)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
