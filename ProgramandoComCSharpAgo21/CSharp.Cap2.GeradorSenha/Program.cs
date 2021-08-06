ff90736266c203e489991d4b313fb76002ba2554
Vinny <vih_leite@hotmail.com>
2021-08-05 21:44:10 -0300

    novo projeto

---
+++ ProgramandoComCSharpAgo21/CSharp.Cap2.GeradorSenha/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Cap2.GeradorSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdeDigitos = 0;
+            bool senhaValida = false;
+
+            while (!senhaValida)
+            {
+                Console.WriteLine("Informe a qtde de dígitos da senha entre 4 e 10: ");
+
+                qtdeDigitos = Convert.ToInt32(Console.ReadLine());
+
+                senhaValida = VerificaDigito(qtdeDigitos, senhaValida);
+            }
+            if (senhaValida)
+            {
+                string novaSenha = String.Empty;
+
+                var rnd = new Random();
+
+                for (int i = 0; i < qtdeDigitos; i++)
+                {
+                    var numRandom = rnd.Next(0, 9);
+                    novaSenha += numRandom;
+                }
+                Console.WriteLine($"Sua senha é {novaSenha}");
+                Console.ReadKey();
+            }
+            
+
+        }
+
+        private static bool VerificaDigito(int qtdeDigitos, bool senhaValida)
+        {
+            if (qtdeDigitos < 4 || qtdeDigitos > 10 || qtdeDigitos % 2 != 0)
+            {
+                Console.WriteLine("Valor de dígitos inválido. Digite um valor válido");
+            }
+            else
+            {
+                senhaValida = true;
+            }
+
+            return senhaValida;
+        }
+    }
+}
